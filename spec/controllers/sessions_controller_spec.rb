1f723d99fc592d3bb39abfa88d55672a1cbc1888
YumShark <yumshark@gmail.com>
2011-06-04 09:10:09 -0400

    Handling invalid sign ins

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,8 @@ describe SessionsController do
   render_views
 
   describe "GET 'new'" do
-    it "should be successful" do
+
+    it "s be successful" do
       get :new
       response.should be_success
     end
@@ -14,6 +15,30 @@ describe SessionsController do
       response.should have_selector('title', :content => "Sign in")
     end
 
+  end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+      before(:each) do
+        @attr = { :email => '', :password => '' }
+      end
+
+      it "s re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "s have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+
+      it "s have the right title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => 'Sign in')
+      end
+    end
   end
 
 end
